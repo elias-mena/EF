dac8f329d58c831b9069dfc376e1668c78780d82
Elias Mena <eliasmena.2911@gmail.com>
2023-05-09 00:07:18 -0600

    Mapeo de Modelos usando DataAnnotations

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace EF.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
+        [Key]
         public Guid CategoriaId {get;set;}
+        [Required]
+        [MaxLength(150)]
         public string Nombre {get;set;}
         public string Descripcion {get;set;}
 
--- Models/Tarea.cs
+++ Models/Tarea.cs
@@ -1,10 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace EF.Models
 {
+[Table("Tareas")]
 public class Tarea
 {
+    [Key]
     public Guid TareaId {get;set;}
+
+    [ForeignKey("CategoriaId")]
     public Guid CategoriaId {get;set;} // Foreign Key
 
+    [Required]
+    [MaxLength(200)]
     public string Titulo {get;set;}
 
     public string Descripcion {get;set;}
@@ -14,6 +23,11 @@ public class Tarea
     public DateTime FechaCreacion {get;set;}
     
     public virtual Categoria Categoria {get;set;}
+
+    [NotMapped] // No se va a mapear en la base de datos
+    public string Resumen {get;set;}
+
+    
 }
 
 public enum Prioridad
