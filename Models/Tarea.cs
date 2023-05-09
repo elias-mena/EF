fd50b253507184833ca37bc0d76f08a645d8f49d
Elias Mena <eliasmena.2911@gmail.com>
2023-05-08 22:34:35 -0600

    Modelos

---
+++ Models/Tarea.cs
@@ -0,0 +1,25 @@
+namespace EF.Models
+{
+public class Tarea
+{
+    public Guid TareaId {get;set;}
+    public Guid CategoriaId {get;set;} // Foreign Key
+
+    public string Titulo {get;set;}
+
+    public string Descripcion {get;set;}
+
+    public Prioridad PrioridadTarea {get;set;}
+
+    public DateTime FechaCreacion {get;set;}
+    
+    public virtual Categoria Categoria {get;set;}
+}
+
+public enum Prioridad
+{
+    Baja,
+    Media,
+    Alta
+}
+}
