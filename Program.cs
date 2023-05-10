d9af0c90629d1b215e114970a646a3c7a0c426db
Elias Mena <eliasmena.2911@gmail.com>
2023-05-09 20:51:57 -0600

    Crear base de datos en memoria

--- Program.cs
+++ Program.cs
@@ -1,6 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using EF;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconection", async ([FromServices] TareasContext db) => 
+{
+    //await db.Database.EnsureCreatedAsync();
+    db.Database.EnsureCreated();
+    return Results.Ok( "Base de datos creada" + db.Database.IsInMemory() );
+});
+
+
+app.MapGet("/categorias", async (TareasContext db) => await db.Categorias.ToListAsync());
+
+//app.MapGet("/tareas", async (TareasContext db) => await db.Tareas.ToListAsync());
 
 app.Run();
--- TareasContext.cs
+++ TareasContext.cs
@@ -11,6 +11,6 @@ namespace EF
         public DbSet<Categoria> Categorias {get;set;}
         public DbSet<Tarea> Tareas {get;set;}
 
-
+        public TareasContext(DbContextOptions<TareasContext> options) :base(options) { }
     }
 }
