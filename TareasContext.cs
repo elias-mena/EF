3c273b374590d814a535127a09d057e45bd3ec2c
Elias Mena <eliasmena.2911@gmail.com>
2023-05-10 03:04:15 -0600

    Configuración de los modelos con Fluent API

--- TareasContext.cs
+++ TareasContext.cs
@@ -12,5 +12,30 @@ namespace EF
         public DbSet<Tarea> Tareas {get;set;}
 
         public TareasContext(DbContextOptions<TareasContext> options) :base(options) { }
+
+        // El método OnModelCreating es el que se encarga de crear el modelo de la base de datos.
+        // Aquí se pueden agregar configuraciones adicionales para el modelo de la base de datos.
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categoria>(categoria=>
+            {
+                categoria.ToTable("Categoria");
+                categoria.HasKey(c=>c.CategoriaId);
+                categoria.Property(c=>c.Nombre).HasMaxLength(150).IsRequired();
+                categoria.Property(c=>c.Descripcion).HasMaxLength(500);
+
+            });
+
+            modelBuilder.Entity<Tarea>(tarea=>
+            {
+                tarea.ToTable("Tarea");
+                tarea.HasKey(t=>t.TareaId);
+                tarea.HasOne(t=>t.Categoria).WithMany(c=>c.Tareas).HasForeignKey(t=>t.CategoriaId);
+                tarea.Property(t=>t.Titulo).HasMaxLength(150).IsRequired();
+                tarea.Property(t=>t.Descripcion).HasMaxLength(500);
+                tarea.Property(t=>t.PrioridadTarea).IsRequired();
+                tarea.Property(t=>t.FechaCreacion).IsRequired();
+            });
+        }
     }
 }
